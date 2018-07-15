6b29cdd44dc2ad417919a8989dce5b197d303a64
sumtimeswhy <moises.r.robles@gmail.com>
2018-07-15 16:09:10 -0700

    Add hello world type functionality No JSX yet.

---
+++ js/ClientApp.jsx
@@ -0,0 +1,26 @@
+const ce = React.createElement;
+
+const MyTitle = function (props) {
+  return ce('div', null, ce('h1', {
+    style: {
+      color: props.color
+    }
+  }, props.title));
+};
+
+const MyFirstComponent = function () {
+  return ce(
+    'div',
+    null,
+    ce(MyTitle, {
+      title: 'Game of Thrones',
+      color: 'YellowGreen'
+    }),
+    ce(MyTitle, {
+      title: 'Stranger Things',
+      color: 'peru'
+    })
+  );
+};
+
+ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
