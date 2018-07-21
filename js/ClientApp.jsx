fe620a467a6ec1957907f0e186a691248b2a4715
sumtimeswhy <moises.r.robles@gmail.com>
2018-07-21 16:50:26 -0700

    Add config files for babel and webpack

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const ce = React.createElement;
 
 const MyTitle = function (props) {
@@ -23,4 +26,4 @@ const MyFirstComponent = function () {
   );
 };
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById('app'));
