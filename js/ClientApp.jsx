49f797d674898e6036ba1997ea0b3b8b5d41ee8b
sumtimeswhy <moises.r.robles@gmail.com>
2018-07-23 15:46:06 -0700

    Refactor ClientApp to use JSX. Modify eslint rules to remove unused var errors

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-
 const MyTitle = function (props) {
-  return ce('div', null, ce('h1', {
-    style: {
-      color: props.color
-    }
-  }, props.title));
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>{props.title}</h1>
+    </div>);
 };
 
 const MyFirstComponent = function () {
-  return ce(
-    'div',
-    null,
-    ce(MyTitle, {
-      title: 'Game of Thrones',
-      color: 'YellowGreen'
-    }),
-    ce(MyTitle, {
-      title: 'Stranger Things',
-      color: 'peru'
-    }),
-    ce(MyTitle, {
-      title: 'The Americans',
-      color: 'blue'
-    })
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Game of Thrones" color="YellowGreen" />
+      <MyTitle title="Stranger Things" color="Peru" />
+      <MyTitle title="The Americans" color="Burlywood" />
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<MyFirstComponent />, document.getElementById('app'));
