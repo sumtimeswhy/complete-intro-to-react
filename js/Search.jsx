9cc2f4d4721e94bf77d82fcf581f6bf336de06b9
sumtimeswhy <moises.r.robles@gmail.com>
2018-08-19 16:08:42 -0700

    Added styled components

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,7 +5,9 @@ import ShowCard from './ShowCard';
 //<pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
 const Search = () => (
   <div className='search'>
-    {preload.shows.map((show) => <ShowCard show={show} />)};
+    <div>
+      {preload.shows.map((show) => <ShowCard key={show.imdbID} show={show} />)};
+    </div>
   </div>
 );
 
