58baba8e207fc528dfb7ecf740840b4f94751838
sumtimeswhy <moises.r.robles@gmail.com>
2018-08-19 16:42:46 -0700

    Added search header

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-//<pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
-const Search = () => (
-  <div className='search'>
-    <div>
-      {preload.shows.map((show) => <ShowCard key={show.imdbID} show={show} />)};
-    </div>
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchTerm: 'debug'
+    };
+
+    this.handleSearch = this.handleSearch.bind(this);
+  };
+
+  handleSearch(event) {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  render() {
+    return (
+      <div className='search'>
+        <header>
+          <h1>svideo</h1>
+          <input type='text' onChange={this.handleSearch} value={this.state.searchTerm} placeholder='Search' />
+        </header>
+
+        <div>
+          {preload.shows.map((show) => <ShowCard key={show.imdbID} show={show} />)};
+        </div>
+      </div>
+    )
+  }
+};
 
 export default Search;
