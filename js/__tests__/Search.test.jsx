fb166cf8057280b6b08a46a26f568cc956744727
sumtimeswhy <moises.r.robles@gmail.com>
2018-09-16 16:29:58 -0700

    Add two tests for search component

--- js/__tests__/Search.test.jsx
+++ js/__tests__/Search.test.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Search from '../Search';
+import ShowCard from '../ShowCard';
+import preload from '../../data.json';
 
 test('Search renders correctly', () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
+});
+
+test('Search should render correct amount of shows', () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+test('Search should render correct amount of shows based on search term', () => {
+  const component = shallow(<Search />);
+  component.setState({ searchTerm: 'black' });
+  expect(component.find(ShowCard).length).toEqual(2);
 });
